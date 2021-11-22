037b3a23fa610218454fd3faca9c894231397ab9
Ander De Abrisqueta Rangel <ander.deabrisqueta.alu@iescampanillas.com>
2021-11-22 22:04:24 +0100

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  priority: string[] = ['Hard drive 1', 'Hard drive 2', 'USB', 'CD ROM', 'PXE'];
 
   constructor() {}
 
+  ngOnInit() {
+    this.priority = this.priority;
+  }
+
+  upBiosPriorityMovement(index: number) {
+    let aux = this.priority[index];
+    this.priority[index] = this.priority[index - 1];
+    this.priority[index - 1] = aux;
+    
+  }
+
+  downBiosPriorityMovement(index: number) {
+    let aux = this.priority[index];
+    this.priority[index] = this.priority[index + 1];
+    this.priority[index + 1] = aux;
+    
+  }
+ 
 }
